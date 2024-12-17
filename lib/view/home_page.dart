a9b9c6471d59814076d4d9d15b8478db486ff30d
Lichyo <lichyo003@gmail.com>
2024-12-17 13:01:03 +0800

    update homepage UI avoid cal page open keyboard render overflow

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -40,6 +40,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       bottomNavigationBar: CurvedNavigationBar(
         color: const Color(0xFF121212),
         index: _currentIndex,
